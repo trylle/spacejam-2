5f9b18ac1379c6b517c0d1f2fc22e59f87c1251d
Thomas Karlsson <thomas.karlsson@bouvet.no>
2021-10-23 02:56:40 +0200

    added 3d models

--- Assets/FireExtinguisher.cs
+++ Assets/FireExtinguisher.cs
@@ -9,23 +9,42 @@ public class FireExtinguisher : MonoBehaviour
     Vector3 mousePos;
     Vector3 playerPos;
     public float Force = 5f;
+    public ParticleSystem particles;
+    public bool Enabled = false;
 
     private void Start()
     {
         player = GameObject.Find("Player");
         rb = GetComponent<Rigidbody>();
+        particles.Stop();
+    }
+
+    public void StartExtinguisher()
+    {
+        enabled = true;
+        particles.Play();
+    }
+
+    public void StopExtinguisher()
+    {
+        enabled = false;
+        particles.Stop(); 
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        playerPos = player.transform.position;
-        RaycastHit hit;
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (enabled)
+        {
+            playerPos = player.transform.position;
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
-        if (!Physics.Raycast(ray, out hit)) return;
-        Vector3 direction = (playerPos - hit.point);
-        direction.y = 0;
-        rb.velocity = direction.normalized * Force;
+            if (!Physics.Raycast(ray, out hit)) return;
+            Vector3 direction = (playerPos - hit.point);
+            direction.y = 0;
+            rb.velocity = direction.normalized * Force;
+        }
+
     }
 }
